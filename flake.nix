155181d092e4616be13c0bf289614566bf230923
DragonHuntrX <John@Devost.net>
2025-04-22 14:38:06 -0400

    made a set not a function

--- flake.nix
+++ flake.nix
@@ -13,54 +13,46 @@
       flake-parts,
       ...
     }:
-    flake-parts.lib.mkFlake { inherit inputs; } (
-      top@{
-        config,
-        withSystem,
-        mod,
-        ...
-      }:
-      {
-        imports = [ ];
-        flakoe = {
-          overlays = rec {
-            alacritty-theme = final: prev: {
-              alacritty-theme = self.packages.${prev.system};
-            };
-            default = alacritty-theme;
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      flakoe = {
+        overlays = rec {
+          alacritty-theme = final: prev: {
+            alacritty-theme = self.packages.${prev.system};
           };
-
+          default = alacritty-theme;
         };
 
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-        ];
+      };
 
-        perSystem =
-          { lib, pkgs, ... }:
-          let
-            themeDir = ./themes;
-            tomls = builtins.attrNames (builtins.readDir themeDir);
-            mkThemes =
-              name:
-              let
-                raw = builtins.readFile "${themeDir}/${name}";
-                data = builtins.fromTOML raw;
-                base = lib.removeSuffix ".toml" name;
-              in
-              {
-                inherit base;
-                value = data.colors;
-              };
-            themes = lib.listToAttrs (map mkThemes tomls);
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-          in
-          {
-            packages = themes;
+      perSystem =
+        { lib, pkgs, ... }:
+        let
+          themeDir = ./themes;
+          tomls = builtins.attrNames (builtins.readDir themeDir);
+          mkThemes =
+            name:
+            let
+              raw = builtins.readFile "${themeDir}/${name}";
+              data = builtins.fromTOML raw;
+              base = lib.removeSuffix ".toml" name;
+            in
+            {
+              inherit base;
+              value = data.colors;
+            };
+          themes = lib.listToAttrs (map mkThemes tomls);
 
-          };
-      }
-    );
+        in
+        {
+          packages = themes;
+
+        };
+    };
 
 }

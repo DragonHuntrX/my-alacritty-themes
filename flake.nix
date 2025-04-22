19a353e4c4b9a3b23855e15a4b4140cb799db0db
DragonHuntrX <John@Devost.net>
2025-04-22 12:06:21 -0400

    First commit; repo init

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "This is my overlay for the alacritty themes!";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.simpleFlake {
+      inherit self;
+      inherit nixpkgs;
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      commonOverlays = [
+        (
+          final: prev:
+          let
+            lib = nixpkgs.lib;
+            themeDir = ./themes;
+            tomls = builtins.attrNames (builtins.readDir themeDir);
+            mkThemes =
+              name:
+              let
+                raw = builtins.readFile "${themeDir}/${name}";
+                data = builtins.fromTOML raw;
+                base = lib.removeSuffix ".toml" name;
+              in
+              {
+                inherit base;
+                value = data.colors;
+              };
+            themes = lib.listToAttrs (map mkThemes tomls);
+          in
+          {
+            myAlacrittyThemes = themes;
+          }
+        )
+      ];
+    };
+}
